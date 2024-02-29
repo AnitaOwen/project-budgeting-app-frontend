4dc8425e33401ef0eb84fc9487949bb4c229a4ee
AnitaOwen <anitaowen@pursuit.org>
2024-02-29 17:41:48 -0500

    added colored text to total based on amount

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -2,11 +2,7 @@ import { Link } from "react-router-dom"
 
 const NavBar = ({ transactions }) => {
 
-  const total = transactions.reduce((acc, current) => {
-    acc += current.amount
-    // console.log(acc)
-    return acc
-}, 0)
+  const total = transactions.reduce((acc, current) => acc += current.amount, 0)
 
   return (
     <div className="nav-wrapper">
@@ -14,7 +10,7 @@ const NavBar = ({ transactions }) => {
         <h1>BUDGETR</h1>
       </div>
       <div className="header-right">
-        <h3>Account Total: <span className={total > 0 ? "positive-text" : "negative-text"}>${total}</span></h3>
+        <h3>Account Total: <span className={total > 100 ? "green-text" : total < 0 ? "red-text" : "yellow-text"}>${total}</span></h3>
         <Link to="/new">
             <button>NEW TRANSACTION</button>
         </Link>
